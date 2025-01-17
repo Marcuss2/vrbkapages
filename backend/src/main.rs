992e7dd6c8bd459748c84311455b176f2224eaa8
Marek Vrbka <marekvrbka@gmail.com>
2025-01-17 21:18:34 +0100

    feat: Make the dist directory configurable

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,13 @@
-use std::time::Duration;
 use axum::extract::MatchedPath;
 use axum::http::{Request, StatusCode};
-use axum::response::{Html, IntoResponse, Response};
+use axum::response::{IntoResponse, Response};
 use axum::routing::{get, get_service};
 use axum::Router;
+use std::time::Duration;
 use tokio::net::TcpListener;
 use tower_http::classify::ServerErrorsFailureClass;
-use tower_http::trace::TraceLayer;
 use tower_http::services::ServeDir;
+use tower_http::trace::TraceLayer;
 use tracing::{event, info, info_span, Level, Span};
 
 async fn health_check() -> impl IntoResponse {
@@ -18,7 +18,9 @@ async fn health_check() -> impl IntoResponse {
 async fn main() -> Result<(), std::io::Error> {
     tracing_subscriber::fmt::init();
 
-    let static_files_service = get_service(ServeDir::new("../dist"));
+    let static_files_service = get_service(ServeDir::new(
+        std::env::var("DIST_DIRECTORY").unwrap_or("../dist".into()),
+    ));
 
     let app = Router::new()
         .fallback_service(static_files_service)
@@ -39,10 +41,14 @@ async fn main() -> Result<(), std::io::Error> {
                     )
                 })
                 .on_request(|request: &Request<_>, _span: &Span| {
-                    event!(Level::INFO, "Request received.");
+                    event!(Level::INFO, "Request {} received.", request.method());
                 })
                 .on_response(|_response: &Response, latency: Duration, _span: &Span| {
-                    event!(Level::INFO, "Response sent, took {}ms.", latency.as_millis());
+                    event!(
+                        Level::INFO,
+                        "Response sent, took {}ms.",
+                        latency.as_millis()
+                    );
                 })
                 .on_failure(
                     |error: ServerErrorsFailureClass, _latency: Duration, _span: &Span| {
